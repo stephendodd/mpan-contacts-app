6f05f12a63d93ead2bb020ec4fbc60e2592d2f9f
Stephen Dodd <stephen.dodd@rea-group.com>
2017-02-26 10:42:46 +1100

    Populating real mpan data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // TODO: Polyfill fetch
 
-var url = "https://sheets.googleapis.com/v4/spreadsheets/1YgppTK_-AKbUv2rn_QFa6gqvF0qG3OFB15OeMULM4Tw/values/2%20Col?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
+//var url = "https://sheets.googleapis.com/v4/spreadsheets/1YgppTK_-AKbUv2rn_QFa6gqvF0qG3OFB15OeMULM4Tw/values/2%20Col?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
+var url = "https://sheets.googleapis.com/v4/spreadsheets/177rO3sZmxC_9-91UPf9JjOZzjfpokCOnk_oBSMiUcOA/values/Sheet1?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
+
 // var url = "https://sheets.googleapis.com/v4/spreadsheets/1tiTL3IHbEDIjKYH-C1_j-C7t_E0OJSUJAiDWffXvmWw/values/MAJOR%20TV?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
 var contactEmails = "";
 var emailLink = document.getElementById('email');
@@ -23,11 +25,13 @@ var data = {}
 data[headers[0].toLowerCase()] = row[0]
 data[headers[1].toLowerCase()] = row[1]
 data[headers[2].toLowerCase()] = row[2]
+
 contactEmails += row[1]+','
+
 
     return '\
 <tr>\
-  <td>' + data.display + '</td>\
+  <td>' + data.contact + '</td>\
   <td><a href="mailto:' + data.email + '">' + data.email + '</a></td>\
 </tr>\
 ';
