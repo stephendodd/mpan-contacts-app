bb3d0653a94f627229c4345a6dd3b7700730908c
Stephen Dodd <stephen.dodd@rea-group.com>
2017-02-25 16:40:44 +1100

    Remove first header object in array

--- index.js
+++ index.js
@@ -3,16 +3,23 @@
 var url = "https://sheets.googleapis.com/v4/spreadsheets/1YgppTK_-AKbUv2rn_QFa6gqvF0qG3OFB15OeMULM4Tw/values/2%20Col?key=AIzaSyCem77u6PE4zsEvs87PJIe7Wj2xERXP6Mk"
 
 fetch(url)
-  .then(function(response) { return response.json() })
+  .then(parseResponse)
   .then(renderResponse);
 
+function parseResponse(response) {
+  return response.json()
+}
+
 function renderResponse(json) {
-  console.log(json.values);
-  var data = []
 
   //Massage data
+  var headers = json.values.shift()
+  console.log(headers);
   var trs = json.values.map(function(row) {
-    var data = {"display": row[0], "email": row[1], "state": row[2]};
+var data = {}
+data[headers[0].toLowerCase()] = row[0]
+data[headers[1].toLowerCase()] = row[1]
+data[headers[2].toLowerCase()] = row[2]
 
     return '\
 <tr>\
