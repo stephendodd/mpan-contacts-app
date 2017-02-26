9090ea5f11d671029fa56b42e489745b25ed206e
Stephen Dodd <stephen.dodd@rea-group.com>
2017-02-26 12:35:54 +1100

    Store all data coming from google spreadsheet in allData variable

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // TODO: Polyfill fetch
 
-//var url = "https://sheets.googleapis.com/v4/spreadsheets/1YgppTK_-AKbUv2rn_QFa6gqvF0qG3OFB15OeMULM4Tw/values/2%20Col?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
-var url = "https://sheets.googleapis.com/v4/spreadsheets/177rO3sZmxC_9-91UPf9JjOZzjfpokCOnk_oBSMiUcOA/values/Sheet1?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
-
-// var url = "https://sheets.googleapis.com/v4/spreadsheets/1tiTL3IHbEDIjKYH-C1_j-C7t_E0OJSUJAiDWffXvmWw/values/MAJOR%20TV?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
+var url = "https://sheets.googleapis.com/v4/spreadsheets/177rO3sZmxC_9-91UPf9JjOZzjfpokCOnk_oBSMiUcOA/values/major%20tv?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
 var contactEmails = "";
 var emailLink = document.getElementById('email');
+var allData = []
 
 fetch(url)
   .then(parseResponse)
@@ -16,15 +14,19 @@ function parseResponse(response) {
 }
 
 function renderResponse(json) {
-
   //Massage data
   var headers = json.values.shift()
-  console.log(headers);
   var trs = json.values.map(function(row) {
 var data = {}
+
 data[headers[0].toLowerCase()] = row[0]
 data[headers[1].toLowerCase()] = row[1]
 data[headers[2].toLowerCase()] = row[2]
+data[headers[3].toLowerCase()] = row[3]
+data[headers[4].toLowerCase()] = row[4]
+data[headers[5].toLowerCase()] = row[5]
+
+allData.push(data)
 
 contactEmails += row[1]+','
 
