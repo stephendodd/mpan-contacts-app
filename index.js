c90b1a67690eb589880f9d77f16e24982ba72dd2
Stephen Dodd <stephen.dodd@rea-group.com>
2017-02-26 13:54:55 +1100

    Refactor render data function

--- index.js
+++ index.js
@@ -2,45 +2,57 @@
 
 var url = "https://sheets.googleapis.com/v4/spreadsheets/177rO3sZmxC_9-91UPf9JjOZzjfpokCOnk_oBSMiUcOA/values/major%20tv?key=AIzaSyC1DObwIS_U1F3YA_ePQFd2Bn50cLmXb-U"
 var contactEmails = "";
-var emailLink = document.getElementById('email');
+
 var allData = []
 
 fetch(url)
-  .then(parseResponse)
-  .then(renderResponse);
+.then(parseResponse)
+.then(getData);
 
 function parseResponse(response) {
   return response.json()
 }
 
-function renderResponse(json) {
+function getData(json) {
   //Massage data
   var headers = json.values.shift()
   var trs = json.values.map(function(row) {
-var data = {}
+    var data = {}
 
-data[headers[0].toLowerCase()] = row[0]
-data[headers[1].toLowerCase()] = row[1]
-data[headers[2].toLowerCase()] = row[2]
-data[headers[3].toLowerCase()] = row[3]
-data[headers[4].toLowerCase()] = row[4]
-data[headers[5].toLowerCase()] = row[5]
+    data[headers[0].toLowerCase()] = row[0]
+    data[headers[1].toLowerCase()] = row[1]
+    data[headers[2].toLowerCase()] = row[2]
+    data[headers[3].toLowerCase()] = row[3]
+    data[headers[4].toLowerCase()] = row[4]
+    data[headers[5].toLowerCase()] = row[5]
 
-allData.push(data)
+    allData.push(data)
+  });
 
-contactEmails += row[1]+','
+  renderTable(allData);
+  renderEmailLink(allData);
 
-    return '\
-<tr>\
-  <td>' + data.contact + '</td>\
-  <td><a href="mailto:' + data.email + '">' + data.email + '</a></td>\
-</tr>\
-';
-  })
-
-  document.getElementById("tablebody").innerHTML = (trs.join("\n"));
 }
 
-emailLink.onclick = function() {
-  this.href = "mailto:"+contactEmails
+function renderTable(tableData) {
+  var trs = tableData.reduce(function(acc ,data) {
+    return (
+      acc +
+      '<tr>' +
+        '<td>' + data.contact + '</td>' +
+        '<td><a href="mailto:' + data.email + '">' + data.email + '</a></td>' +
+      '</tr>'
+    );
+  }, "");
+  document.getElementById("tablebody").innerHTML = trs;
+
+}
+
+function renderEmailLink(tableData) {
+  var emailArray = tableData.map(function(data) {
+    return data.email
+  });
+  var mailToURI = 'mailto:' + emailArray.join(',');
+
+  document.getElementById('email').href = mailToURI;
 }
